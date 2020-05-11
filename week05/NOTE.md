a452666e42e28b40291bb7e1118ea57751730ec8
muyifang1 <muyifang1@gmail.com>
2020-05-12 00:06:50 +0800

    a part of homework, client.js send() need work continue.

--- week05/NOTE.md
+++ week05/NOTE.md
@@ -1,1 +1,10 @@
 # 每周总结可以写在这里
+// 通过xhr验证server.js
+var xhr = new XMLHttpRequest;
+xhr.open("get","http://127.0.0.1:8088",true);
+xhr.send(null);
+xhr.responseText
+
+// 返回结果 "ok"
+
+// 需要学习 xhr
---
+++ week05/client.js
@@ -0,0 +1,82 @@
+const net = require("net");
+
+class Request{
+  // method, url = host + port + path
+  // body: k/v
+  // headers
+  constructor(options){
+    this.method = options.method || "GET";
+    this.host = options.host;
+    this.port = options.port || 80;
+    this.path = options.path || "/";
+    this.body = options.body || {};
+    this.headers = options.headers || {};
+
+    if(!this.headers["Content-Type"]){
+        this.headers["Content-Type"] = "application/x-www-form-urlencoded"
+    }
+
+    if(this.headers["Content-Type"] === "application/json"){
+        this.bodyText = JSON.stringify(this.body);
+    }else if(this.headers["Content-Type"] === "application/x-www-form-urlencoded"){
+        this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+        // 计算Content-Length
+        this.headers["Content-Length"] = this.bodyText.length;
+    }
+  }
+
+  toString(){
+      return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}`
+  }
+// todo need work continue 01:13:04
+  send(){
+
+  }
+}
+
+class Response{
+
+}
+
+const client = net.createConnection({
+  host:"127.0.0.1",
+  port: 8088 }, () => {
+  // 'connect' listener.
+  console.log('connected to server!');
+
+  let request = new Request({
+      method:"POST",
+      host:"127.0.0.1",
+      port:"8088",
+      path:"/",
+      headers: {
+        ["X-Foo2"]:"customed"
+      },
+      body:{
+          name:"yangqi"
+      }
+  })
+
+  console.log(request.toString());
+  client.write(request.toString());
+
+  // 模拟post请求
+/*
+  client.write('POST /HTTP/1.1\r\n');
+  client.write('Content-Type: application/x-www-form-urlencoded\r\n');
+  client.write('Content-Length: 11\r\n');
+  client.write('\r\n');
+  client.write('name=yangqi');
+*/
+//  client.write("POST /HTTP/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 11\r\n\r\nname=winter");
+});
+client.on('data', (data) => {
+  console.log(data.toString());
+  client.end();
+});
+client.on('end', () => {
+  console.log('disconnected from server');
+});
